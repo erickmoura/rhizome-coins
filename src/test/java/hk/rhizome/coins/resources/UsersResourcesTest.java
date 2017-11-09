0091d25c2438ccb0e2733cac056652a1114e28c4
Omaida Gonzalez <omi.gongar@gmail.com>
2017-11-08 20:59:14 -0500

    tests

--- src/test/java/hk/rhizome/coins/resources/UsersResourcesTest.java
+++ src/test/java/hk/rhizome/coins/resources/UsersResourcesTest.java
@@ -7,8 +7,11 @@ import hk.rhizome.coins.db.UserExchangesDAO;
 import hk.rhizome.coins.db.UsersDAO;
 import hk.rhizome.coins.logger.AppLogger;
 import hk.rhizome.coins.model.Exchanges;
+import hk.rhizome.coins.model.User;
 import hk.rhizome.coins.model.UserBalances;
 import hk.rhizome.coins.model.UserOrders;
+import hk.rhizome.coins.model.UserTrades;
+
 import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -31,19 +34,20 @@ public class UsersResourcesTest {
 
     //@Test
     public void getBalancesNoForceReload() throws Exception {
-        int userID = 1;
         String collect = "2017-10-23 10:00:00";
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Date collectD = sdf.parse(collect);
-        List<Exchanges> mockedExchanges = getMockedExchanges();
-        List<UserBalances> mockedBalances = getMockedUserBalances();
+        Set<Exchanges> mockedExchanges = getMockedExchanges();
+        Set<UserBalances> mockedBalances = getMockedUserBalances();
         
+        User user = mock(User.class);
+        when(user.getExchanges()).thenReturn(mockedExchanges);
         UsersDAO usersDAO = mock(UsersDAO.class);
         UserExchangesDAO usersExchangesDAO = mock(UserExchangesDAO.class);
-        //when(usersDAO.getExchanges(userID)).thenReturn(mockedExchanges);
+        when(usersDAO.getByID(1)).thenReturn(user);
         
         UsersResources resources = new UsersResources(usersDAO);
-        //when(usersDAO.getBalances(userID, collectD)).thenReturn(mockedBalances);
+        when(user.getBalances()).thenReturn(mockedBalances);
         
         Map<String, Set<UserBalances>> data = resources.getBalances(collect,Optional.empty());
         Assert.assertEquals(data.keySet().size(), 1);
@@ -51,23 +55,24 @@ public class UsersResourcesTest {
         Assert.assertEquals(data.get("Poloniex").size(), 1);
     }
 
-   // @Test
+    @Test
     public void getOrdersNoForceReload() throws Exception {
-        int userID = 1;
         String start = "2017-10-23 10:00:00";
         String end = "2017-10-23 10:00:00";
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Date startDate = sdf.parse(start);
         Date endDate = sdf.parse(end);
-        List<Exchanges> mockedExchanges = getMockedExchanges();
-        List<UserOrders> mockedOrders = getMockedUserOrders();
+        Set<Exchanges> mockedExchanges = getMockedExchanges();
+        Set<UserOrders> mockedOrders = getMockedUserOrders();
         
+        User user = mock(User.class);
+        when(user.getExchanges()).thenReturn(mockedExchanges);
         UsersDAO usersDAO = mock(UsersDAO.class);
         UserExchangesDAO usersExchangesDAO = mock(UserExchangesDAO.class);
-        //when(usersDAO.getExchanges(userID)).thenReturn(mockedExchanges);
+        when(usersDAO.getByID(1)).thenReturn(user);
         
         UsersResources resources = new UsersResources(usersDAO);
-        //when(usersDAO.getOrders(userID, startDate, endDate)).thenReturn(mockedOrders);
+        when(user.getOrders()).thenReturn(mockedOrders);
         
         Map<String, Set<UserOrders>> data = resources.getOrders(start, end, Optional.empty());
         Assert.assertEquals(data.keySet().size(), 1);
@@ -75,34 +80,63 @@ public class UsersResourcesTest {
         Assert.assertEquals(data.get("Poloniex").size(), 2);
     }
 
-    //@Test
-    public void getExchanges() throws Exception {
-        int userID = 1;
-        List<Exchanges> mockedList = getMockedExchanges();
+    @Test
+    public void getUserTradesNoForceLoad() throws Exception {
+
+        String start = "2017-10-23 10:00:00";
+        String end = "2017-10-23 10:00:00";
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date startDate = sdf.parse(start);
+        Date endDate = sdf.parse(end);
         
+        Set<Exchanges> mockedExchanges = getMockedExchanges();
+        Set<UserTrades> mockedTrades = getMockedUserTrades();
+        
+        User user = mock(User.class);
+        when(user.getExchanges()).thenReturn(mockedExchanges);
         UsersDAO usersDAO = mock(UsersDAO.class);
         UserExchangesDAO usersExchangesDAO = mock(UserExchangesDAO.class);
-        //when(usersDAO.getExchanges(userID)).thenReturn(mockedList);
+        when(usersDAO.getByID(1)).thenReturn(user);
+        
+        UsersResources resources = new UsersResources(usersDAO);
+        when(user.getTrades()).thenReturn(mockedTrades);
+        
+        Map<String, Set<UserTrades>> data = resources.getTrades(start, end, Optional.empty());
+        Assert.assertEquals(data.keySet().size(), 1);
+        Assert.assertEquals(data.get("Poloniex"), mockedTrades);
+        Assert.assertEquals(data.get("Poloniex").size(), 1);
+        System.out.println(data);
+    }
+
+    @Test
+    public void getExchanges() throws Exception {
+        Set<Exchanges> mockedSet = getMockedExchanges();
+        
+        User u = mock(User.class);
+        UsersDAO usersDAO = mock(UsersDAO.class);
+        UserExchangesDAO usersExchangesDAO = mock(UserExchangesDAO.class);
+        when(usersDAO.getByID(1)).thenReturn(u);
+        when(u.getExchanges()).thenReturn(mockedSet);
         
         UsersResources resources = new UsersResources(usersDAO);
         Set<Exchanges> exchanges = resources.getExchanges();
 
-        Assert.assertEquals(mockedList, exchanges);
+        Assert.assertEquals(mockedSet, exchanges);
     }
 
-    public List<Exchanges> getMockedExchanges(){
-        List<Exchanges> exchanges = new ArrayList<Exchanges>();
+    public Set<Exchanges> getMockedExchanges(){
+        Set<Exchanges> exchanges = new HashSet<Exchanges>();
         Exchanges ex1 = new Exchanges(1, "Poloniex", "org.knowm.xchange.poloniex.PoloniexExchange",	new BigDecimal(0.25), new BigDecimal(0.15),	20);
         exchanges.add(ex1);
         return exchanges;
     }
 
-    public List<UserOrders> getMockedUserOrders() throws Exception{
+    public Set<UserOrders> getMockedUserOrders() throws Exception{
         String start = "2017-10-23 10:00:00";
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Date d = sdf.parse(start);
 
-        List<UserOrders> orders = new ArrayList<UserOrders>();
+        Set<UserOrders> orders = new HashSet<UserOrders>();
         UserOrders o1 = new UserOrders("12", 1, 1, "ETH/BTC", "ASK", "PENDING_NEW", new BigDecimal(222.22), BigDecimal.ZERO, BigDecimal.ZERO, d);
         UserOrders o2 = new UserOrders("34", 1, 1, "ETH/BTC", "BID", "PENDING_NEW", new BigDecimal(262.00), BigDecimal.ZERO, BigDecimal.ZERO, d);
         orders.add(o1);
@@ -110,15 +144,22 @@ public class UsersResourcesTest {
         return orders;
     }
 
-    public List<UserBalances> getMockedUserBalances() throws Exception{
+    public Set<UserBalances> getMockedUserBalances() throws Exception{
         String start = "2017-10-23 10:00:00";
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Date d = sdf.parse(start);
 
-        List<UserBalances> balances = new ArrayList<UserBalances>();
+        Set<UserBalances> balances = new HashSet<UserBalances>();
         UserBalances b1 = new UserBalances(1, 1, "ETH/BTC", new BigDecimal(2322.23), new BigDecimal(2322.23), BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO, d);
         balances.add(b1);
         return balances;
+    }
+
+    public Set<UserTrades> getMockedUserTrades(){
+        UserTrades t = new UserTrades(1, 1, "3304477", "115369066954132", "SC/BTC", new BigDecimal(0.00000040625), "Bitcoin", new BigDecimal(50),new BigDecimal(0.00000325),  new Date(), "ASK");
+        Set<UserTrades> trades = new HashSet<UserTrades>();
+        trades.add(t);
+        return trades;
     }
 
    
