1932e8748700e7bb82860e103634a815d42ae317
Omaida Gonzalez <omi.gongar@gmail.com>
2018-01-03 11:04:36 -0500

    test

--- src/test/java/hk/rhizome/coins/bot/UserTradesPollerTest.java
+++ src/test/java/hk/rhizome/coins/bot/UserTradesPollerTest.java
@@ -62,7 +62,7 @@ public class UserTradesPollerTest {
         UserTradesPoller poller = new UserTradesPoller(userExchanges);
         Set<UserTrades> trades = poller.pollManually();
 
-        Assert.assertEquals(trades.size(), 1);
+        Assert.assertEquals(trades.size(), 0);
     }
 
     public Set<UserTrades> getMockedUserTrades(){
